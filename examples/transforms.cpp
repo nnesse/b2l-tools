6dd4e29fc086d4833b78fd74c42c9fff9b140c28
Neils Nesse <nnesse@sonic.net>
2014-12-18 01:02:40 -0800

    Add a transformed text example

---
+++ examples/transforms.cpp
@@ -0,0 +1,152 @@
+/*
+
+Copyright (c) 2014 Neils Nesse
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+#include "gltext.hpp"
+
+#include <GLFW/glfw3.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include <iostream>
+
+static void error_callback(int error, const char* description)
+{
+	fputs(description, stderr);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	gl_text::renderer renderer;
+
+	glfwSetErrorCallback(error_callback);
+
+	if (!glfwInit())
+		exit(-1);
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+#if GLTEXT_USE_GLEW
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+#else
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
+
+	window = glfwCreateWindow(512, 512, "Transformed text example", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		exit(-1);
+	}
+
+	glfwMakeContextCurrent(window);
+
+#if GLTEXT_USE_GLEW
+	glewInit();
+#else
+	glbindify::gl::init();
+#endif
+
+	const char *charset = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ()'\"0123456789`~!@#$%^&*()_+;/?.>,<={}[]\\";
+	const gl_text::font *fonts[1];
+	gl_text::font_desc font_desc[1] = {
+		{
+			.typeface = renderer.get_typeface("ttf/LiberationSans-Bold.ttf"),
+			.family = "LiberationSans",
+			.style = gl_text::STYLE_BOLD,
+			.width = 30,
+			.height = 30,
+			.charset = charset
+		}
+	};
+	if (!renderer.initialize(font_desc, 1, fonts)) {
+		glfwTerminate();
+		exit(-1);
+	}
+
+	glfwSetKeyCallback(window, key_callback);
+
+#if GLTEXT_USE_GLEW
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+#else
+	glbindify::glEnable(GL_BLEND);
+	glbindify::glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+#endif
+
+	glfwSwapInterval(1);
+
+	float theta = 0;
+	while (!glfwWindowShouldClose(window)) {
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+#if GLTEXT_USE_GLEW
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT);
+#else
+		glbindify::glViewport(0, 0, width, height);
+		glbindify::glClear(GL_COLOR_BUFFER_BIT);
+#endif
+		theta += 0.03f;
+
+		// Rotatate the text
+		float mvp[] = {
+			sinf(theta)*2.0f/width, cosf(theta)*2.0f/height, 0, 0,
+			cosf(theta)*2.0f/width, -sinf(theta)*2.0f/height, 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1};
+
+		// Scale the text
+		for (int i = 0; i < 8; i++) {
+			mvp[i] *= 1.0f + sinf(theta);
+		}
+
+		// Center the text
+		mvp[12] -= (mvp[0] * width / 2.0f) + (mvp[4] * height / 2.0f);
+		mvp[13] -= (mvp[1] * width / 2.0f) + (mvp[5] * height / 2.0f);
+
+		renderer.render(fonts[0],
+			gl_text::color(1,1,1,1),
+			"Transformed text\n",
+			mvp,	
+			width, height, /* Occupy whole viewport */
+			gl_text::HALIGN_CENTER, gl_text::VALIGN_CENTER /* horizontally and vertically center text */);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
