5b55764253ec76287c91b7312c6a1c790e66b1cd
Niels Nesse <nnesse@sonic.net>
2015-09-05 01:44:23 -0700

    Simplify event processing

--- examples/simple.c
+++ examples/simple.c
@@ -5,49 +5,6 @@
 #include "gltext.h"
 #include "glb-glcore.h"
 
-gltext_renderer_t g_renderer;
-gltext_font_t g_font;
-
-void on_expose(struct glplatform_win *win)
-{
-	int width, height;
-	width = win->width;
-	height = win->height;
-	glViewport(0, 0, width, height);
-	glClearColor(0,0,0,1);
-	glClear(GL_COLOR_BUFFER_BIT);
-	float mvp[16] = {
-		2.0/width,0,0,0,
-		0,-2.0/height,0,0,
-		0,0,1,0,
-		-1,0,0,1};
-
-	const char *str = "The quick brown fox jumps over the lazy dog()'\"0123456789`~!@#$%^&*()_+;/?.>,<={}[]\\";
-	struct gltext_glyph_instance *r = gltext_renderer_prepare_render(g_renderer, g_font, strlen(str));
-
-	const struct gltext_glyph *g_prev = NULL;
-	float x_pos = 0;
-	float y_pos = 0;
-	struct gltext_color color = {
-		.r = 1,
-		.g = 1,
-		.b = 1,
-		.a = 1
-	};
-	while (*str) {
-		const struct gltext_glyph *g_cur = gltext_get_glyph(g_font, *str);
-		x_pos += gltext_get_advance(g_prev, g_cur);
-		r->pos[0] = x_pos;
-		r->pos[1] = y_pos;
-		r->w = g_cur->w;
-		r++;
-		str++;
-		g_prev = g_cur;
-	}
-	gltext_renderer_submit_render(g_renderer, &color, mvp);
-	glplatform_swap_buffers(win);
-}
-
 void on_destroy(struct glplatform_win *win)
 {
 	glplatform_destroy_window(win);
@@ -55,12 +12,14 @@ void on_destroy(struct glplatform_win *win)
 
 int main()
 {
+	gltext_renderer_t renderer;
+	gltext_font_t font;
+
 	struct glplatform_win_callbacks cb = {
-		.on_expose = on_expose,
 		.on_destroy = on_destroy
 	};
 
-	g_renderer = gltext_renderer_new(" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'\"0123456789`~!@#$%^&*()_+;/?.>,<={}[]\\");
+	renderer = gltext_renderer_new(" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'\"0123456789`~!@#$%^&*()_+;/?.>,<={}[]\\");
 
 	if (!glplatform_init()) {
 		fprintf(stderr, "Failed to initialize GL window manager\n");
@@ -81,14 +40,52 @@ int main()
 	glplatform_make_current(win, ctx);
 	glb_glcore_init(3, 3);
 
-	g_font = gltext_font_create(g_renderer, gltext_renderer_get_typeface(g_renderer, TTF_PATH "/LiberationSans-Regular.ttf"), 20);
-	if (!g_font) {
+	font = gltext_font_create(renderer, gltext_renderer_get_typeface(renderer, TTF_PATH "/LiberationSans-Regular.ttf"), 20);
+	if (!font) {
 		fprintf(stderr, "Failed to create font\n");
 		exit(-1);
 	}
 
 	while (glplatform_process_events()) {
+		int width, height;
+		width = win->width;
+		height = win->height;
+		glViewport(0, 0, width, height);
+		glClearColor(0,0,0,1);
+		glClear(GL_COLOR_BUFFER_BIT);
+		float mvp[16] = {
+			2.0/width,0,0,0,
+			0,-2.0/height,0,0,
+			0,0,1,0,
+			-1,0,0,1};
+
+		const char *str = "The quick brown fox jumps over the lazy dog()'\"0123456789`~!@#$%^&*()_+;/?.>,<={}[]\\";
+		struct gltext_glyph_instance *r = gltext_renderer_prepare_render(renderer, font, strlen(str));
+
+		const struct gltext_glyph *g_prev = NULL;
+		float x_pos = 0;
+		float y_pos = 0;
+		struct gltext_color color = {
+			.r = 1,
+			.g = 1,
+			.b = 1,
+			.a = 1
+		};
+		while (*str) {
+			const struct gltext_glyph *g_cur = gltext_get_glyph(font, *str);
+			x_pos += gltext_get_advance(g_prev, g_cur);
+			r->pos[0] = x_pos;
+			r->pos[1] = y_pos;
+			r->w = g_cur->w;
+			r++;
+			str++;
+			g_prev = g_cur;
+		}
+		gltext_renderer_submit_render(renderer, &color, mvp);
+		glplatform_swap_buffers(win);
+
 		if (glplatform_get_events(true) < 0)
 			break;
 	}
+	return 0;
 }
